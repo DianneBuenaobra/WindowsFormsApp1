f3b622318a765894ebf7eef8b6c14ae5619d73ed
Buenaobra.258124 <Buenaobra.258124@LAB-B-PC22>
2023-09-15 18:38:32 +0800

    Add project files.

---
+++ WindowsFormsApp1/QueuingForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class QueuingForm : Form
+    {
+        private CashierClass cashier;
+        public QueuingForm()
+        {
+            
+            InitializeComponent();
+        
+        }
+
+        private void btnCashier_Click(object sender, EventArgs e)
+        {
+            lblQueue.Text = cashier.CashierGeneratedNumber("P - ");
+            CashierClass.getNumberInQueue = lblQueue.Text;
+            CashierClass.CashierQueue.Enqueue(CashierClass.getNumberInQueue);
+        }
+    }
+}
+ 
